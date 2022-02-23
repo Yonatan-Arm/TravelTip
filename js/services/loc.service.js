ef8f13da6c507ca6047449cec084555615603215
yosefzemertov <yosizemertov@gmail.com>
2022-02-23 17:52:47 +0200

    service loc

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,12 +2,15 @@ export const locService = {
     getLocs
 }
 
-const GEO_URL ='https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY'
+import { storageService } from './local-storage.js'
+const KEY = 'mapLocation'
+const GEO_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY'
 const API_KEY = 'AIzaSyDVaWRaXe4Fxq65Ws1-ZcA7fhz2ENG2L1g'
-
+var selectedLocIdx;
+var gLocs = (storageService.load(KEY).length) ? storageService.load(KEY) : [];
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'q', lat: 32.047201, lng: 34.832581 }
 ]
 
@@ -25,8 +28,8 @@ function createNewLoc(name, Let, lng) {
     var loc = {
         id: Math.random().toString(16).slice(2),
         name: name,
-        let: Let,
-        lng: lng,
+        pos:{let: Let,
+            lng: lng},
         createdAt: time,
         update: null
     }
@@ -34,9 +37,19 @@ function createNewLoc(name, Let, lng) {
     storageService.save(KEY, gLocs)
 }
 
+function getLocs(){
+    return gLocs;
+}
 
-function getLocById(id){
+
+function getLocById(id) {
     var locIdx = gLocs.findIndex(loc => loc.id === id)
+    selectedLocIdx = locIdx;
     return gLocs[locIdx];
+}
+
+function removeLoc(id) {
+    getLocById(id);
+    gLocs.splice(selectedLocIdx, 1)
 }
 
