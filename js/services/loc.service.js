a0f388c43e5ff366f6dd8acef9d7097c926603fe
yosefzemertov <yosizemertov@gmail.com>
2022-02-23 14:22:19 +0200

    loc service

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,6 +2,8 @@ export const locService = {
     getLocs
 }
 
+const GEO_URL ='https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY'
+const API_KEY = 'AIzaSyDVaWRaXe4Fxq65Ws1-ZcA7fhz2ENG2L1g'
 
 const locs = [
     { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
