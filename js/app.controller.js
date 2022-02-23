5612cb2a0cff6d53eb5a7a89d49393519cdeb177
Yonatan arm <yoniarm123@gmail.com>
2022-02-23 19:15:41 +0200

    fix

--- js/app.controller.js
+++ js/app.controller.js
@@ -11,11 +11,12 @@ window.onGetUserPos = onGetUserPos;
 function onInit() {
     mapService.initMap()
         .then(() => {
+           
             console.log('Map is ready');
         })
         .catch(() => console.log('Error: cannot init map'));
         
-
+ renderLocation(locations)
 }
 
 
@@ -57,3 +58,55 @@ function onPanTo() {
     console.log('Panning the Map');
     mapService.panTo(35.6895, 139.6917);
 }
+
+function onGoLocation(){
+
+
+
+}
+
+
+function onDeleteLocation(locationIdx){
+
+
+}
+
+function renderLocation(locations){
+    let elLocations=document.querySelector('.locations-table')
+    var strHtml=` <table class = "table">
+   <tr> <th> id </th>
+    <th> Name </th>
+    <th> pos </th>
+    <th> createdAt </th>
+    <th> update </th>
+    <th> Actions </th>
+    </tr>`
+    locations.map(location => {
+        strHtml += `<tbody><tr><td> ${location.id} </td>
+            <td>  ${location.name} </td>
+            <td> ${location.pos}</td>
+            <td> ${location.createdAt}</td>
+            <td> ${location.update}</td>
+            <td>
+            <button onclick='onGoLocation(${location.pos})'> Go </button>
+            <button onclick='onDeleteLocation(${location.id})'> Delete Location </button>
+        </td></tr>`
+
+    })
+    strHtml += '</tbody></table>'
+    elLocations.innerHTML = strHtml
+
+}
+
+
+
+var locations= [{  id: '1',
+    name:  'Home',
+    pos: 'lat : 31 , lng : 34',
+    createdAt: Date.now(),
+    update: 'AA'},
+{  id: '2',
+    name:  'Work',
+    pos: 'lat : 31.2 , lng : 34.2',
+    createdAt:  Date.now(),
+    update: 'AA'}]
